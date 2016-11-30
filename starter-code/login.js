8e73814de164193dd546616619fdfdf0374be8cf
ricarellano <rarellanob@gmail.com>
2016-11-29 23:48:44 -0800

    adds control flow training

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,14 @@
 console.log("login.js loaded");
+var userLogin = {
+  userName: "ricardo",
+  password: "match"
+};
+
+var pass = prompt("Enter password for user" + " " + userLogin.userName );
+
+if (userLogin.password == pass) {
+   alert("correct password!");
+}
+else {
+  prompt("Enter their password again");
+}
--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,19 @@
 console.log("security_questions.js loaded");
+
+var securityQuestions = [{
+  question: "What is your favorite movie?",
+  expectedAnswer: "into the wild"
+},
+{
+  question: "What was your favorite food as a child?",
+  expectedAnswer: "pizza"
+}];
+
+
+for (var i = 0; i < securityQuestions.length; i++) {
+  var answer = prompt(securityQuestions[i].question);
+   if (answer != securityQuestions[i].expectedAnswer){
+      alert("Stop asking questions!");
+     break;
+   }
+}
